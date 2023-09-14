fb14cedd3b55d0869a3c23518232b7bd73f6722f
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:39:22 -0300

    Update Rest_EndSleeping.cs

--- VisualStudio/Patches/Rest_EndSleeping.cs
+++ VisualStudio/Patches/Rest_EndSleeping.cs
@@ -1,11 +1,11 @@
-﻿namespace AuroraMonitor
-{
-    [HarmonyPatch(typeof(Rest), nameof(Rest.EndSleeping), new Type[] { typeof(bool) })]
-    public class Rest_EndSleeping
-    {
-        public static void Postfix()
-        {
-            Main.ShowWeatherAlert();
-        }
-    }
-}
+﻿//namespace AuroraMonitor
+//{
+//    [HarmonyPatch(typeof(Rest), nameof(Rest.EndSleeping), new Type[] { typeof(bool) })]
+//    public class Rest_EndSleeping
+//    {
+//        public static void Postfix()
+//        {
+//            MessageUtilities.Instance.ShowWeatherAlert();
+//        }
+//    }
+//}

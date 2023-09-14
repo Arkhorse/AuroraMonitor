e4a951a27bb6aaa3b6c412a0248493ce06895c78
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:38:48 -0300

    Create Aurora.cs

---
+++ VisualStudio/Utilities/Aurora.cs
@@ -0,0 +1,22 @@
+﻿namespace AuroraMonitor.Utilities
+{
+    public class Aurora
+    {
+        private static AuroraManager manager = GameManager.GetAuroraManager();
+        private static Il2Cpp.Weather weatherComponent = GameManager.GetWeatherComponent();
+        public static void SetAuroraChancesEarly(int early)
+        {
+            weatherComponent.m_AuroraEarlyWindowProbability = early;
+        }
+        public static void SetAuroraChancesLate(int late)
+        {
+            weatherComponent.m_AuroraLateWindowProbability = late;
+        }
+
+        public static void SetAuroraChances(int early, int late)
+        {
+            weatherComponent.m_AuroraEarlyWindowProbability = early;
+            weatherComponent.m_AuroraLateWindowProbability = late;
+        }
+    }
+}

0f67b8936c48a8adc240e2c124710b2104b714f3
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:38:30 -0300

    Create Icons.cs

---
+++ VisualStudio/WeatherNotificationPanel/Icons/Icons.cs
@@ -0,0 +1,14 @@
+﻿namespace AuroraMonitor.WeatherNotificationPanel.WeatherIcons
+{
+    public class WeatherIconsHolder
+    {
+        public string ID { get; set; }
+        public GameObject Prefab { get; set; }
+
+        public WeatherIconsHolder(string ID, GameObject Prefab)
+        {
+            this.ID = ID;
+            this.Prefab = Prefab;
+        }
+    }
+}

860b69cb2fabf633e40c936dba3d74e73feadc8b
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:38:10 -0300

    Create AssetBundleUtilities.cs

---
+++ VisualStudio/Utilities/AssetBundleUtilities.cs
@@ -0,0 +1,21 @@
+﻿namespace AuroraMonitor.Utilities
+{
+    public class AssetBundleUtilities
+    {
+        public static AssetBundle? LoadBundle(string path)
+        {
+            AssetBundle? temp;
+            MemoryStream? memory;
+            Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            memory = new((int)stream?.Length);
+            stream.CopyTo(memory);
+
+            temp = AssetBundle.LoadFromMemory(memory.ToArray());
+
+            memory.Dispose();
+            stream.Dispose();
+
+            return temp;
+        }
+    }
+}

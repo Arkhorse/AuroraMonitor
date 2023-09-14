32b8cd3c7d44a971ca594c5b2840aaf91de6ea70
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:39:12 -0300

    Update WeatherTransition_ChooseNextWeatherSet.cs

--- VisualStudio/Patches/WeatherTransition_ChooseNextWeatherSet.cs
+++ VisualStudio/Patches/WeatherTransition_ChooseNextWeatherSet.cs
@@ -1,13 +1,38 @@
-﻿namespace AuroraMonitor
-{
-    [HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ChooseNextWeatherSet))]
-    internal class WeatherTransition_ChooseNextWeatherSet
-    {
-        private static void Postfix()
-        {
-            if (GameManager.GetExperienceModeManagerComponent().IsChallengeActive()) return; // mod not usable in challenges
-            if (Main.GetCurrentWeatherLoc == null) return;
-            Main.ShowNewWeatherAlert();
-        }
-    }
-}
+﻿//namespace AuroraMonitor
+//{
+//    [HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ActivateWeatherSet), new Type[] { typeof(WeatherStage) })]
+//    public class WeatherTransition_ActivateWeatherSet
+//    {
+//        public static void Postfix(WeatherTransition __instance, ref WeatherSet reqType)
+//        {
+//            MessageUtilities.Instance.ShowWeatherAlert(true);
+//            Utilities.SavedStage = __instance.m_CurrentWeatherSet.m_CharacterizingType;
+
+//            if (Settings.Instance.PRINTDEBUGLOG) Logger.Log("Activated message via: WeatherTransition.ActivateWeatherSet(WeatherSet reqType)");
+//        }
+//    }
+
+//    [HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ActivateWeatherSet), new Type[] { typeof(string) })]
+//    public class WeatherTransition_ActivateWeatherSet_custom
+//    {
+//        public static void Postfix(WeatherTransition __instance, ref string customTypeName)
+//        {
+//            MessageUtilities.Instance.ShowWeatherAlert(true);
+//            Utilities.SavedStage = __instance.m_CurrentWeatherSet.m_CharacterizingType;
+
+//            if (Settings.Instance.PRINTDEBUGLOG) Logger.Log("Activated message via: WeatherTransition.ActivateWeatherSet(tring customTypeName)");
+//        }
+//    }
+
+//    [HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ActivateWeatherSetAtFrac), new Type[] { typeof(WeatherStage), typeof(float) })]
+//    public class WeatherTransition_ActivateWeatherSetAtFrac
+//    {
+//        public static void Postfix(WeatherTransition __instance, ref WeatherSet reqType, ref float startAtFrac)
+//        {
+//            MessageUtilities.Instance.ShowWeatherAlert(true);
+//            Utilities.SavedStage = __instance.m_CurrentWeatherSet.m_CharacterizingType;
+
+//            if (Settings.Instance.PRINTDEBUGLOG) Logger.Log("Activated message via: WeatherTransition.ActivateWeatherSetAtFrac(WeatherSet reqType, float startAtFrac)");
+//        }
+//    }
+//}

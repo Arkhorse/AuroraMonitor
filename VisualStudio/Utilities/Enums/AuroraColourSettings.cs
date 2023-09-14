444a6e3cff7b6ad44c331420e0c2dbf5c90d4982
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:37:42 -0300

    Create AuroraColourSettings.cs

---
+++ VisualStudio/Utilities/Enums/AuroraColourSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AuroraMonitor.Utilities.Enums
+{
+    public enum AuroraColourSettings { Default, Cinematic, Custom };
+    /*
+        Cinematic #6496fa   : 0.392156869f, 0.5882353f, 0.980392158f, 1f
+        Default             : 0.3, 0.2, 0.35, 0
+    */
+}

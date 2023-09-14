e54542d955fe8a08a02c2739213ea3758076de08
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-14 18:38:14 -0300

    Create BetterAurora.cs

---
+++ VisualStudio/WeatherSets/BetterAurora.cs
@@ -0,0 +1,19 @@
+﻿using Il2Cpp;
+
+namespace AuroraMonitor.WeatherSets
+{
+    public class BetterAurora : MonoBehaviour
+    {
+        public BetterAurora() : base() { }
+        public BetterAurora(IntPtr intPtr) : base(intPtr) { }
+        private static Weather GetWeather { get; }              = GameManager.GetWeatherComponent();
+        private static UniStormWeatherSystem unistorm { get; }  = GameManager.GetUniStorm();
+        private static Wind wind { get; }                       = GameManager.GetWindComponent();
+        public void Activate()
+        {
+            wind.StartPhaseImmediate(WindDirection.North, WindStrength.Calm);
+            unistorm.m_Temperature = (int)AuroraSettings.Instance.BetterAuroraTemperature;
+            this.enabled = true;
+        }
+    }
+}

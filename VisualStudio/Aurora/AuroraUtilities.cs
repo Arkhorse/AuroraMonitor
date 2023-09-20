cafbe80044ce5a99c4a0ce54eccb65c4acf31a31
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-20 14:52:32 -0300

    Create AuroraUtilities.cs

---
+++ VisualStudio/Aurora/AuroraUtilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraMonitor.Aurora
+{
+    public class AuroraUtilities
+    {
+		/// <summary>
+		/// Used to fetch aurora time. Currently not implemented
+		/// </summary>
+		internal static void FetchAuroraTime()
+        {
+            Logger.Log($"Aurora Time Left: {GameManager.GetAuroraManager().GetNormalizedAlpha()}");
+        }
+
+        /// <summary>
+        /// Used to fetch aurora colour for the log
+        /// </summary>
+        internal static void FetchAuroraColour()
+        {
+            Color AuroraColor = GameManager.GetAuroraManager().GetAuroraColour();
+            Logger.Log($"Aurora Color: R:{AuroraColor.r} G:{AuroraColor.g} B:{AuroraColor.b} A:{AuroraColor.a}");
+        }
+
+		public static void SetAuroraChancesEarly( int early )
+		{
+			GameManager.GetWeatherComponent().m_AuroraEarlyWindowProbability = early;
+		}
+		public static void SetAuroraChancesLate( int late )
+		{
+			GameManager.GetWeatherComponent().m_AuroraLateWindowProbability = late;
+		}
+		public static void SetAuroraChances( int early, int late )
+		{
+			GameManager.GetWeatherComponent().m_AuroraEarlyWindowProbability = early;
+			GameManager.GetWeatherComponent().m_AuroraLateWindowProbability = late;
+		}
+	}
+}

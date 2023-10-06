2cdb90024b9bea7aac6f993dbd34f36471ea081f
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-06 03:42:15 -0300

    Update AuroraMonitor.cs

--- VisualStudio/AuroraMonitor.cs
+++ VisualStudio/AuroraMonitor.cs
@@ -1,21 +1,24 @@
 global using AuroraUtilities = AuroraMonitor.Aurora.AuroraUtilities;
 global using WeatherNotifications = AuroraMonitor.Notifications.WeatherNotifications;
 global using WeatherUtilities = AuroraMonitor.Utilities.WeatherUtilities;
-
+global using AuroraMonitor.Utilities;
+using AuroraMonitor.Notifications;
 using MelonLoader.Utils;
 
 namespace AuroraMonitor
 {
     internal class Main : MelonMod
     {
-        public static string MonitorFolder                      = Path.Combine(MelonEnvironment.ModsDirectory, "Monitor");
-        public static bool BaseLoaded { get; set; }             = false;
-        public static bool SandboxLoaded { get; set; }          = false;
-        public static bool DLC01Loaded { get; set; }            = false;
-        public static string BaseSceneName { get; set; }        = string.Empty;
-        public static string SandboxSceneName { get; set; }     = string.Empty;
-        public static string DLC01SceneName { get; set; }       = string.Empty;
-        public static bool ModInitiliated { get; set; }         = false;
+        public static string MonitorFolder { get; } = Path.Combine(MelonEnvironment.ModsDirectory, "Monitor");
+        public static string MonitorMainConfig { get; } = Path.Combine(MonitorFolder, "main.json");
+        public static bool BaseLoaded { get; set; } = false;
+        public static bool SandboxLoaded { get; set; } = false;
+        public static bool DLC01Loaded { get; set; } = false;
+        public static string BaseSceneName { get; set; } = string.Empty;
+        public static string SandboxSceneName { get; set; } = string.Empty;
+        public static string DLC01SceneName { get; set; } = string.Empty;
+        public static bool ModInitiliated { get; set; } = false;
+        public static WeatherMonitorData MonitorData { get; set; } = new();
 
         public override void OnInitializeMelon()
         {
@@ -27,22 +30,28 @@ namespace AuroraMonitor
             }
         }
 
+        public override void OnDeinitializeMelon()
+        {
+            base.OnDeinitializeMelon();
+        }
+
         // Using this actually works 100% of the time. OnSceneWasLoaded and OnSceneWasUnloaded is Additative scene loads ONLY
         public override void OnSceneWasInitialized(int buildIndex, string sceneName)
         {
             base.OnSceneWasInitialized(buildIndex, sceneName);
-			if ( sceneName == "Empty" || sceneName == "Boot" || sceneName.StartsWith( "MainMenu", StringComparison.InvariantCultureIgnoreCase ) ) return;
+            if (sceneName == "Empty" || sceneName == "Boot" ) return;
 
-			bool RegionOrZone = sceneName.Contains("Region", StringComparison.InvariantCultureIgnoreCase) || sceneName.Contains("Zone", StringComparison.InvariantCultureIgnoreCase);
-            bool IsAdditiveScene = sceneName.Contains("SANDBOX", StringComparison.InvariantCultureIgnoreCase) || sceneName.EndsWith("DARKWALKER", StringComparison.InvariantCultureIgnoreCase) || sceneName.EndsWith("DLC01", StringComparison.InvariantCultureIgnoreCase);
+            if (sceneName.StartsWith("MainMenu", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Settings.Instance.OnLoadConfirm();
+                return;
+            }
 
             // only needed if you want to know what the scene name is. The buildIndex is always -1 or 0
-            if (Settings.Instance.PRINTDEBUGLOG) Logger.Log($"Scene Initialized, name: {sceneName}, index: {buildIndex}");
+            //if (Settings.Instance.PRINTDEBUGLOG) Logging.Log($"Scene Initialized, name: {sceneName}, index: {buildIndex}");
 
-            // prevents a null ref error
-            if (sceneName == "Empty" || sceneName == "Boot" || sceneName.StartsWith("MainMenu", StringComparison.InvariantCultureIgnoreCase)) return;
 
-            if (RegionOrZone && !IsAdditiveScene)
+            if (SceneUtilities.IsSceneBase(sceneName) && !SceneUtilities.IsSceneAdditive(sceneName))
             {
                 BaseLoaded = true;
                 BaseSceneName = sceneName;
@@ -59,29 +68,11 @@ namespace AuroraMonitor
                 DLC01Loaded = true;
                 DLC01SceneName = sceneName;
             }
-
-            //SceneHandle.OnSceneWasLoaded(sceneName);
         }
 
         public override void OnLateUpdate()
         {
             base.OnLateUpdate();
-
-            if (GameManager.GetWeatherComponent() && !ModInitiliated)
-            {
-                if (Settings.Instance.AuroraChanceRemember)
-                {
-                    AuroraUtilities.SetAuroraChances(Settings.Instance.AuroraChanceEarly, Settings.Instance.AuroraChanceLate);
-                }
-                else if (!Settings.Instance.AuroraChanceRemember)
-                {
-                    Settings.Instance.AuroraChanceEarly = GameManager.GetWeatherComponent().m_AuroraEarlyWindowProbability;
-                    Settings.Instance.AuroraChanceLate = GameManager.GetWeatherComponent().m_AuroraLateWindowProbability;
-                }
-
-                ModInitiliated = true;
-            }
-
             try
             {
                 string currentScene = GameManager.m_ActiveScene;
@@ -98,7 +89,7 @@ namespace AuroraMonitor
 
             try
             {
-				WeatherNotifications.MaybeDisplayWeatherNotification();
+                WeatherNotifications.MaybeDisplayWeatherNotification();
             }
             catch { }
         }

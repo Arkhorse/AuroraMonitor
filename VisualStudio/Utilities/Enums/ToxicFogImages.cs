dd0edd6be0c4d23995653be6c471d8896434402b
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-14 20:34:51 -0300

    Create ToxicFogImages.cs

---
+++ VisualStudio/Utilities/Enums/ToxicFogImages.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraMonitor.Utilities.Enums
+{
+    public enum ToxicFogImages { l1, l2, l3 }
+}

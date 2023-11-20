012a4278afa361ca08ca347e8e826feeaee89604
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-11-19 21:56:12 -0400

    Create MainConfig.cs

---
+++ VisualStudio/Data/MainConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraMonitor.Data
+{
+    public class MainConfig : IDataBase
+    {
+        public bool BaseLoaded { get; set; }
+        public bool SandboxLoaded { get; set; }
+        public bool DLC01Loaded { get; set; }
+        public string? BaseSceneName { get; set; }
+        public string? SandboxSceneName { get; set; }
+        public string? DLC01SceneName { get; set; }
+        public bool ModInitiliated { get; set; }
+    }
+}

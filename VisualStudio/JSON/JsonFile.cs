ea5e3fc4844ad27b539817bdd7e7c6c9016cd261
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:19:04 -0300

    Update JsonFile.cs

--- VisualStudio/JSON/JsonFile.cs
+++ VisualStudio/JSON/JsonFile.cs
@@ -1,103 +1,184 @@
-﻿using AuroraMonitor.Utilities.Logger;
+﻿// ---------------------------------------------
+// JsonFile - by The Illusion
+// ---------------------------------------------
+// Reusage Rights ------------------------------
+// You are free to use this script or portions of it in your own mods, provided you give me credit in your description and maintain this section of comments in any released source code
+//
+// Warning !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Ensure you change the namespace to whatever namespace your mod uses, so it doesnt conflict with other mods
+// ---------------------------------------------
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace AuroraMonitor.JSON
+namespace AuroraMonitor.Utilities.JSON
 {
-    public class JsonFile
-    {
-        public static JsonSerializerOptions DefaultOptions { get; } = new JsonSerializerOptions()
-        {
-            WriteIndented = true, // pretty print
-            IncludeFields = true // use [JsonInclude] on properties you want to include, otherwise it wont be
-        };
+	/// <summary>
+	/// 
+	/// </summary>
+	public class JsonFile
+	{
+		/// <summary>
+		/// Gets the default set of options
+		/// </summary>
+		/// <returns></returns>
+		public static JsonSerializerOptions GetDefaultOptions()
+		{
+			return new JsonSerializerOptions()
+			{
+				WriteIndented = true,   // pretty print
+				IncludeFields = true    // use [JsonInclude] on properties you want to include, otherwise it wont be
+			};
+		}
 
-        #region Syncronous
-        public static void Save<T>(string configFileName, T? Tinput, JsonSerializerOptions? options = null)
-        {
-            try
-            {
-                options ??= DefaultOptions;
-                using FileStream file = File.Open(configFileName, FileMode.Create, FileAccess.Write, FileShare.None);
-                JsonSerializer.Serialize<T?>(file, Tinput, options);
-                file.Dispose();
-            }
-            catch (Exception e)
-            {
-                Main.Logger.Log(FlaggedLoggingLevel.Critical, $"Attempting to save {configFileName} failed", e);
-            }
-        }
+		#region Syncronous
+		/// <summary>
+		/// Save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static void Save<T>(string configFileName, T Tinput)
+		{
+			Save<T>(configFileName, Tinput, GetDefaultOptions());
+		}
+		/// <summary>
+		/// Save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static void Save<T>(string configFileName, T Tinput, JsonSerializerOptions? options = null)
+		{
+			try
+			{
+				options ??= GetDefaultOptions();
+				using FileStream file = File.Open(configFileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+				JsonSerializer.Serialize<T>(file, Tinput, options);
+				file.Dispose();
+			}
+			catch (System.Exception e)
+			{
+				throw new BadMemeException($"Attempting to save {configFileName} failed", e);
+			}
+		}
 
-        public static T? Load<T>(string configFileName, JsonSerializerOptions? options = null)
-        {
-            try
-            {
-                options ??= DefaultOptions;
-                using FileStream file = File.Open(configFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-                T? output = JsonSerializer.Deserialize<T?>(file, options);
-                file.Dispose();
-                return output;
-            }
-            catch
-            {
-                Main.Logger.Log(FlaggedLoggingLevel.Critical, $"Attempting to load {configFileName} failed");
-                throw;
-            }
-        }
-        #endregion
-        #region Async
-        /// <summary>
-        /// Loads a given JSON file
-        /// </summary>
-        /// <typeparam name="T">The class to deserialize</typeparam>
-        /// <param name="configFileName">absolute path to the file</param>
-        /// <returns>new class based on file contents</returns>
-        public static async Task<T?> LoadAsync<T>(string configFileName, JsonSerializerOptions? options = null)
-        {
-            //https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-based-asynchronous-programming
-            //Task taskA = Task.Run( () => Console.WriteLine("Hello from taskA."));
-            //https://stackoverflow.com/questions/38423472/what-is-the-difference-between-task-run-and-task-factory-startnew
-            /*
-                in the .NET Framework 4.5 Developer Preview, we’ve introduced the new Task.Run method. This in no way obsoletes Task.Factory.StartNew,
-                but rather should simply be thought of as a quick way to use Task.Factory.StartNew without needing to specify a bunch of parameters.
-                It’s a shortcut. In fact, Task.Run is actually implemented in terms of the same logic used for Task.Factory.StartNew, just passing in
-                some default parameters. When you pass an Action to Task.Run:
+		/// <summary>
+		/// Load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static T Load<T>(string configFileName)
+		{
+			return Load<T>(configFileName, false, GetDefaultOptions());
+		}
 
-                'Task.Run(someAction);'
+		/// <summary>
+		/// Load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static T Load<T>(string configFileName, bool createFile)
+		{
+			return Load<T>(configFileName, createFile, GetDefaultOptions());
+		}
 
-                it's exactly equivalent to:
+		/// <summary>
+		/// Load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static T Load<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
+		{
+			if (!File.Exists(configFileName))
+			{
+				if (createFile)
+				{
+					var instance = (T)Activator.CreateInstance(typeof(T));
+					Save<T>(configFileName, instance, options);
+				}
+				else
+				{
+					throw new BadMemeException($"JsonFile.Load({configFileName}, {createFile})::Requested JSON file does not exist");
+				}
+			}
+			try
+			{
+				options ??= GetDefaultOptions();
+				using FileStream file = File.Open(configFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+				var output = JsonSerializer.Deserialize<T>(file, options);
+				file.Dispose();
+				return output;
+			}
+			catch (System.Exception e)
+			{
+				throw new BadMemeException($"Attempting to load the config file failed, file: {configFileName}", e);
+			}
+		}
+		#endregion
+		#region Async
+		/// <summary>
+		/// Async load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task<T> LoadAsync<T>(string configFileName, bool createFile = false)
+		{
+			return await LoadAsync<T>(configFileName, createFile, GetDefaultOptions());
+		}
+		/// <summary>
+		/// Async load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task<T> LoadAsync<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
+		{
+			return await Task.Run(() => Load<T>(configFileName, createFile, options));
+		}
 
-                'Task.Factory.StartNew(someAction, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);'
-             */
+		/// <summary>
+		/// Async save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task SaveAsync<T>(string configFileName, T Tinput)
+		{
+			await SaveAsync<T>(configFileName, Tinput, GetDefaultOptions());
+		}
 
-            return await Task.Run(() => Load<T>(configFileName, options));
-        }
-
-        /// <summary>
-        /// Saves a new JSON file
-        /// </summary>
-        /// <typeparam name="T">The class to serialize</typeparam>
-        /// <param name="configFileName">absolute path to the file</param>
-        /// <param name="Tinput">an instance of the given class with information filled</param>
-        public static async Task SaveAsync<T>(string configFileName, T? Tinput, JsonSerializerOptions? options = null)
-        {
-            //https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-based-asynchronous-programming
-            //Task taskA = Task.Run( () => Console.WriteLine("Hello from taskA."));
-            //https://stackoverflow.com/questions/38423472/what-is-the-difference-between-task-run-and-task-factory-startnew
-            /*
-                in the .NET Framework 4.5 Developer Preview, we’ve introduced the new Task.Run method. This in no way obsoletes Task.Factory.StartNew,
-                but rather should simply be thought of as a quick way to use Task.Factory.StartNew without needing to specify a bunch of parameters.
-                It’s a shortcut. In fact, Task.Run is actually implemented in terms of the same logic used for Task.Factory.StartNew, just passing in
-                some default parameters. When you pass an Action to Task.Run:
-
-                'Task.Run(someAction);'
-
-                it's exactly equivalent to:
-
-                'Task.Factory.StartNew(someAction, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);'
-             */
-
-            await Task.Run(() => Save<T>(configFileName, Tinput, options));
-        }
-        #endregion
-    }
+		/// <summary>
+		/// Async save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task SaveAsync<T>(string configFileName, T Tinput, JsonSerializerOptions? options = null)
+		{
+			await Task.Run(() => Save<T>(configFileName, Tinput, options));
+		}
+		#endregion
+	}
 }

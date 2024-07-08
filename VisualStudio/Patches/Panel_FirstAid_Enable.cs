816d691198cae97f676dfca1715f378aec441723
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:25:21 -0300

    Update Panel_FirstAid_Enable.cs

--- VisualStudio/Patches/Panel_FirstAid_Enable.cs
+++ VisualStudio/Patches/Panel_FirstAid_Enable.cs
@@ -1,21 +1,20 @@
-﻿using AuroraMonitor.Utilities.Logger;
-
+﻿using AuroraMonitor.GUI.Addons.FirstAidPanel;
 namespace AuroraMonitor.Patches
 {
-    [HarmonyPatch(typeof(Panel_FirstAid), nameof(Panel_FirstAid.Enable), new Type[] { typeof(bool) })]
-    public class Panel_FirstAid_Enable
-    {
-        private static bool WasInit { get; set; } = false;
-        public static void Prefix(Panel_FirstAid __instance)
-        {
-            if (!__instance.enabled) return;
-            if (!WasInit)
-            {
-                __instance.gameObject.AddComponent<FirstAidPanel_Addons>();
-                WasInit = true;
+	[HarmonyPatch(typeof(Panel_FirstAid), nameof(Panel_FirstAid.Enable), new Type[] { typeof(bool) })]
+	public class Panel_FirstAid_Enable
+	{
+		private static bool WasInit { get; set; } = false;
+		public static void Prefix(Panel_FirstAid __instance)
+		{
+			if (!__instance.enabled) return;
+			if (!WasInit)
+			{
+				__instance.gameObject.AddComponent<WeatherDisplay>();
+				WasInit = true;
 
-                Main.Logger.Log(FlaggedLoggingLevel.Debug, "Added FirstAidPanel_Addons to Panel_FirstAid");
-            }
-        }
-    }
+				Main.Logger.Log("Added WeatherDisplay to Panel_FirstAid", FlaggedLoggingLevel.Debug);
+			}
+		}
+	}
 }

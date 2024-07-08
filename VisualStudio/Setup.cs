d60e392322c72628eb99900f75a6d5b9edf97e0f
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:26:09 -0300

    Update Setup.cs

--- VisualStudio/Setup.cs
+++ VisualStudio/Setup.cs
@@ -1,51 +1,44 @@
 ﻿namespace AuroraMonitor
 {
-    public class Setup
-    {
-        public static bool Init()
-        {
-            SetupInstancedClasses();
-            SetupConfigurationData();
-            return true;
-        }
+	public class Setup
+	{
+		public static bool Init()
+		{
+			SetupInstancedClasses();
+			SetupConfigurationData();
 
-        private static bool SetupFolders()
-        {
-            if (!Directory.Exists(Main.MonitorFolder))
-            {
-                Directory.CreateDirectory(Main.MonitorFolder);
-            }
+			return true;
+		}
 
-            return true;
-        }
+		private static bool SetupFolders()
+		{
+			if (!Directory.Exists(Main.MonitorFolder))
+			{
+				Directory.CreateDirectory(Main.MonitorFolder);
+			}
 
-        private static bool SetupInstancedClasses()
-        {
-            Main.Config ??= new();
-            Main.SettingsInstance ??= new();
-            Main.MonitorData ??= new();
+			return true;
+		}
 
-            return true;
-        }
+		private static bool SetupInstancedClasses()
+		{
+			Main.Config ??= new();
+			Main.SettingsInstance ??= new();
+			Main.MonitorData ??= new();
 
-        private static bool SetupConfigurationData()
-        {
-            if (SetupFolders())
-            {
-                if (!File.Exists(Main.MonitorMainConfig))
-                {
-                    JsonFile.Save<WeatherMonitorData>(Main.MonitorMainConfig, Main.MonitorData);
-                    Main.MonitorData = default;
-                }
-                else
-                {
-                    Main.MonitorData = JsonFile.Load<WeatherMonitorData>(Main.MonitorMainConfig);
-                }
+			return true;
+		}
 
-                return true;
-            }
+		private static bool SetupConfigurationData()
+		{
+			if (SetupFolders())
+			{
+				Main.MonitorData = JsonFile.Load<WeatherMonitorData>(Main.MonitorMainConfig, true);
+				Main.Config = JsonFile.Load<MainConfig>(Main.MonitorConfig, true);
+				return true;
+			}
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 }

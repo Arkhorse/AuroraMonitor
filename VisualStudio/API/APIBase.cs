e971c0fdec42fab00c9a13bbffe7a886e8bf1ad4
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:15:09 -0300

    Create APIBase.cs

---
+++ VisualStudio/API/APIBase.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AuroraMonitor.API
+//{
+//	public abstract class APIBase
+//	{
+//		public abstract void MaybeSaveLocalData();
+//		public abstract bool MaybeLoadLocalData();
+//		public abstract void RegisterEntry();
+//		public abstract void DeRegisterEntry();
+//	}
+//}

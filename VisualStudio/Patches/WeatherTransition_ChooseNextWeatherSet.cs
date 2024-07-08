55bcfbb7d22578d16f9124254bca73b5305bdcb7
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:24:54 -0300

    Update WeatherTransition_ChooseNextWeatherSet.cs

--- VisualStudio/Patches/WeatherTransition_ChooseNextWeatherSet.cs
+++ VisualStudio/Patches/WeatherTransition_ChooseNextWeatherSet.cs
@@ -1,31 +1,15 @@
-﻿/*namespace AuroraMonitor
+﻿namespace AuroraMonitor
 {
-    [HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ChooseNextWeatherSet))]
-    internal class WeatherTransition_ChooseNextWeatherSet
-    {
-        private static void Postfix()
-        {
-            if (GameManager.GetExperienceModeManagerComponent().IsChallengeActive()) return; // mod not usable in challenges
-            string? nextWeather = null;
-            nextWeather = GameManager.GetWeatherComponent().GetWeatherStage() switch
-            {
-                WeatherStage.DenseFog           => "GAMEPLAY_WeatherHeavyFog",
-                WeatherStage.LightSnow          => "GAMEPLAY_WeatherLightSnow",
-                WeatherStage.HeavySnow          => "GAMEPLAY_WeatherHeavySnow",
-                WeatherStage.PartlyCloudy       => "GAMEPLAY_PartlyCloudy",
-                WeatherStage.Clear              => "GAMEPLAY_WeatherClear",
-                WeatherStage.Cloudy             => "GAMEPLAY_Cloudy",
-                WeatherStage.LightFog           => "GAMEPLAY_WeatherLightFog",
-                WeatherStage.Blizzard           => "GAMEPLAY_WeatherBlizzard",
-                WeatherStage.ClearAurora        => "GAMEPLAY_know_th_AuroraObservations1_Title",    // Aurora Borealis
-                WeatherStage.ToxicFog           => "GAMEPLAY_AfflictionToxicFog",                   // Toxic Fog - only darkwalker challenge as of 2.22
-                WeatherStage.ElectrostaticFog   => "GAMEPLAY_ElectrostaticFog",                     // Glimmer Fog
-                WeatherStage.Undefined          => null,
-                _ => null,
-            };
-            if (nextWeather == null) return;
-            Utilities.AuroraMonitorMessage($"Weather Update: {Localization.Get(nextWeather)}", Settings.Main.SettingsInstance.WeatherNotificationsTime );
-        }
-    }
+	[HarmonyPatch(typeof(WeatherTransition), nameof(WeatherTransition.ChooseNextWeatherSet), new Type[] { typeof(WeatherTransitionWeights), typeof(bool), typeof(bool) })]
+	public class WeatherTransition_ChooseNextWeatherSet
+	{
+		public static void Postfix(WeatherTransition __instance, ref WeatherTransitionWeights customWeights, ref bool forceClear, ref bool forceAurora)
+		{
+			if (__instance == null) return;
+			if (Main.Config == null || Main.Config.WeatherWeights == null) return;
+
+			if (forceClear) Main.Logger.Log("Forced Clear", FlaggedLoggingLevel.Debug);
+			if (forceAurora) Main.Logger.Log("Forced Aurora", FlaggedLoggingLevel.Debug);
+		}
+	}
 }
-*/

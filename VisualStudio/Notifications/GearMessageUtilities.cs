574205f13088169bbab8390dfe1d93f04a4507e5
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:23:29 -0300

    Update GearMessageUtilities.cs

--- VisualStudio/Notifications/GearMessageUtilities.cs
+++ VisualStudio/Notifications/GearMessageUtilities.cs
@@ -1,13 +1,11 @@
-﻿using AuroraMonitor.Utilities.Logger;
-
-namespace AuroraMonitor.Notifications
+﻿namespace AuroraMonitor.Notifications
 {
     public class GearMessageUtilities
     {
         public static void AddGearMessage(string prefab, string header, string message, float time)
         {
             GearMessage.AddMessage(prefab, header, message, time);
-            Main.Logger.Log(FlaggedLoggingLevel.Verbose, $"Added GearMessage with the following data: {prefab}, {header}, {message}, {time}");
+            Main.Logger.Log($"Added GearMessage with the following data: {prefab}, {header}, {message}, {time}", ComplexLogger.FlaggedLoggingLevel.Debug);
         }
     }
 }

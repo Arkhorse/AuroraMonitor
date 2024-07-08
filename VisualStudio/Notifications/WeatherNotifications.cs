fda0249516cf598c589de52ec1399bbd83da8f28
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:23:41 -0300

    Update WeatherNotifications.cs

--- VisualStudio/Notifications/WeatherNotifications.cs
+++ VisualStudio/Notifications/WeatherNotifications.cs
@@ -1,104 +1,105 @@
-﻿using AuroraMonitor.JSON;
-using AuroraMonitor.Utilities.Logger;
+﻿//namespace AuroraMonitor.Notifications
+//{
+//    [RegisterTypeInIl2Cpp]
+//    public class WeatherNotifications : MonoBehaviour
+//    {
+//        public WeatherNotifications() { }
+//        public WeatherNotifications(IntPtr intPtr) : base(intPtr) { }
+//        /// <summary>
+//        /// Will do all checks then display the notification
+//        /// </summary>
+//        /// <param name="force">ignore some checks, will not ignore checks that will throw errors</param>
+//        /// <remarks>
+//        /// <para>Checks not ignored:</para>
+//        /// <para><c>GameManager.GetPlayerManagerComponent() == null</c></para>
+//        /// <para><c>GameManager.GetPlayerManagerComponent().m_ControlMode == PlayerControlMode.Locked</c></para>
+//        /// <para><c>WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()) is null</c></para>
+//        /// <para><c>WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm()) is null</c></para>
+//        /// </remarks>
+//        /// <seealso cref="WeatherUtilities.GetCurrentWeatherIcon(UniStormWeatherSystem)"/>
+//        /// <seealso cref="WeatherUtilities.GetCurrentWeatherLoc(UniStormWeatherSystem)"/>
+//        /// <seealso cref="DisplayWeatherNotification"/>
+//        public static void MaybeDisplayWeatherNotification(bool force = false)
+//        {
+//            if (!Main.IsPlayerAvailable(GameManager.GetPlayerManagerComponent())) return;
 
-namespace AuroraMonitor.Notifications
-{
-    [RegisterTypeInIl2Cpp]
-    public class WeatherNotifications : MonoBehaviour
-    {
-        public WeatherNotifications(IntPtr intPtr) : base(intPtr) { }
-        /// <summary>
-        /// Will do all checks then display the notification
-        /// </summary>
-        /// <param name="force">ignore some checks, will not ignore checks that will throw errors</param>
-        /// <remarks>
-        /// <para>Checks not ignored:</para>
-        /// <para><c>GameManager.GetPlayerManagerComponent() == null</c></para>
-        /// <para><c>GameManager.GetPlayerManagerComponent().m_ControlMode == PlayerControlMode.Locked</c></para>
-        /// <para><c>WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()) is null</c></para>
-        /// <para><c>WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm()) is null</c></para>
-        /// </remarks>
-        /// <seealso cref="WeatherUtilities.GetCurrentWeatherIcon(UniStormWeatherSystem)"/>
-        /// <seealso cref="WeatherUtilities.GetCurrentWeatherLoc(UniStormWeatherSystem)"/>
-        /// <seealso cref="DisplayWeatherNotification"/>
-        public static void MaybeDisplayWeatherNotification(bool force = false)
-        {
-            if (!Main.IsPlayerAvailable(GameManager.GetPlayerManagerComponent())) return;
+//            if ( (SceneUtilities.IsValidSceneForWeather(GameManager.m_ActiveScene, Main.SettingsInstance.WeatherNotificationsIndoors) && GameManager.GetUniStorm().m_SecondsSinceLastWeatherChange >= Main.SettingsInstance.WeatherNotificationsDelay ) || force)
+//            {
+//                if (Main.MonitorData != null && (Main.MonitorData.Prev != GameManager.GetUniStorm().m_CurrentWeatherStage) || force)
+//                {
+//                    if (WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()) is null) return;
+//                    if (WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm()) is null) return;
 
-            if ( (SceneUtilities.IsValidSceneForWeather(GameManager.m_ActiveScene, Main.SettingsInstance.WeatherNotificationsIndoors) && GameManager.GetUniStorm().m_SecondsSinceLastWeatherChange >= Main.SettingsInstance.WeatherNotificationsDelay ) || force)
-            {
-                if (Main.MonitorData != null && (Main.MonitorData.Prev != GameManager.GetUniStorm().m_CurrentWeatherStage) || force)
-                {
-                    if (WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()) is null) return;
-                    if (WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm()) is null) return;
+//                    if (BuildWeatherData())
+//                    {
+//                        DisplayWeatherNotification();
+//                        SaveWeatherData();
+//                    }
+//                }
+//            }
+//        }
 
-                    if (BuildWeatherData())
-                    {
-                        DisplayWeatherNotification();
-                        SaveWeatherData();
-                    }
-                }
-            }
-        }
+//        // TODO: Convert to using localizations
+//        private static void DisplayWeatherNotification()
+//        {
+//            string message = Localization.Get(WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()));
 
-        // TODO: Convert to using localizations
-        private static void DisplayWeatherNotification()
-        {
-            string message = Localization.Get(WeatherUtilities.GetCurrentWeatherLoc(GameManager.GetUniStorm()));
+//            GearMessageUtilities.AddGearMessage(
+//                WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm())!,
+//                "Weather Monitor",
+//                $"{message}",
+//                Main.SettingsInstance.WeatherNotificationsTime);
 
-            GearMessageUtilities.AddGearMessage(
-                WeatherUtilities.GetCurrentWeatherIcon(GameManager.GetUniStorm())!,
-                "Weather Monitor",
-                $"{message}",
-                Main.SettingsInstance.WeatherNotificationsTime);
+//            Main.Logger.Log("Weather notification displayed", ComplexLogger.FlaggedLoggingLevel.Debug);
+//        }
 
-            Main.Logger.Log(FlaggedLoggingLevel.Debug, "Weather notification displayed");
-        }
+//        private static bool BuildWeatherData()
+//        {
+//            if (Main.MonitorData == null) return false;
 
-        private static bool BuildWeatherData()
-        {
-            if (Main.MonitorData == null) return false;
+//#pragma warning disable IDE0028 // Simplify collection initialization
+//            Main.MonitorData.m_WeatherInformation ??= new();
+//#pragma warning restore IDE0028 // Simplify collection initialization
 
-            if (Main.MonitorData.m_WeatherInformation == null) Main.MonitorData.m_WeatherInformation = new();
+//            float day = GameManager.GetTimeOfDayComponent().GetDayNumber();
+//            float hour = GameManager.GetTimeOfDayComponent().GetHour();
+//            float minute = GameManager.GetTimeOfDayComponent().GetMinutes();
 
-            float day = GameManager.GetTimeOfDayComponent().GetDayNumber();
-            float hour = GameManager.GetTimeOfDayComponent().GetHour();
-            float minute = GameManager.GetTimeOfDayComponent().GetMinutes();
+//            DayInformation dayInformation = new()
+//            {
+//                Day         = (int)day,
+//                Hour        = (int)hour,
+//                Minute      = (int)minute
+//            };
 
-            DayInformation dayInformation = new()
-            {
-                Day         = (int)day,
-                Hour        = (int)hour,
-                Minute      = (int)minute
-            };
+//            WeatherInformation weatherInformation = new()
+//            {
+//                m_DayInformation    = dayInformation,
+//                m_WeatherStage      = GameManager.GetUniStorm().m_CurrentWeatherStage,
+//                WindAngle           = GameManager.GetWindComponent().GetWindAngleRelativeToPlayer(),
+//                WindPlayerMult      = GameManager.GetPlayerMovementComponent().GetWindMovementMultiplier(),
+//                WindSpeed           = WeatherUtilities.ConvertMilesKilometerHour(GameManager.GetWindComponent().GetSpeedMPH()),
+//                Temperature         = GameManager.GetWeatherComponent().GetBaseTemperature()
+//            };
 
-            WeatherInformation weatherInformation = new()
-            {
-                m_DayInformation    = dayInformation,
-                m_WeatherStage      = GameManager.GetUniStorm().m_CurrentWeatherStage,
-                WindAngle           = GameManager.GetWindComponent().GetWindAngleRelativeToPlayer(),
-                WindPlayerMult      = GameManager.GetPlayerMovementComponent().GetWindMovementMultiplier(),
-                WindSpeed           = WeatherUtilities.ConvertMilesKilometerHour(GameManager.GetWindComponent().GetSpeedMPH()),
-                Temperature         = GameManager.GetWeatherComponent().GetBaseTemperature()
-            };
+//            Main.MonitorData.Prev = GameManager.GetUniStorm().m_CurrentWeatherStage;
 
-            Main.MonitorData.Prev = GameManager.GetUniStorm().m_CurrentWeatherStage;
+//            if (weatherInformation.m_DayInformation.Day != GameManager.GetUniStorm().m_DayCounter)
+//            {
+//                return false;
+//            }
+//            else
+//            {
+//                Main.Logger.Log("New weather data added to database", ComplexLogger.FlaggedLoggingLevel.Debug);
+//                Main.MonitorData.m_WeatherInformation.Add(weatherInformation);
+//                return true;
+//            }
+//        }
 
-            if (weatherInformation.m_DayInformation.Day != GameManager.GetUniStorm().m_DayCounter)
-            {
-                return false;
-            }
-            else
-            {
-                Main.Logger.Log(FlaggedLoggingLevel.Debug, "New weather data added to database");
-                Main.MonitorData.m_WeatherInformation.Add(weatherInformation);
-                return true;
-            }
-        }
-
-        private static void SaveWeatherData()
-        {
-            JsonFile.Save<WeatherMonitorData>(Main.MonitorMainConfig, Main.MonitorData);
-        }
-    }
-}
+//        private static void SaveWeatherData()
+//        {
+//            JsonFile.Save<WeatherMonitorData>(Main.MonitorMainConfig, Main.MonitorData);
+//			JsonFile.Save<MainConfig>(Main.MonitorConfig, Main.Config);
+//        }
+//    }
+//}

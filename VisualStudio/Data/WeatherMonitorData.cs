99fd0b8713059db482003ea2dbb1926523668e87
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:16:35 -0300

    Update WeatherMonitorData.cs

--- VisualStudio/Data/WeatherMonitorData.cs
+++ VisualStudio/Data/WeatherMonitorData.cs
@@ -4,47 +4,53 @@ using AuroraMonitor.Utilities.Exceptions;
 
 namespace AuroraMonitor.Data
 {
-    public class WeatherMonitorData
-    {
-        [JsonInclude]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        [JsonPropertyName("Previous Weather Stage")]
-        public WeatherStage Prev { get; set; }
+	public class WeatherMonitorData
+	{
+		public WeatherMonitorData() { }
 
-        [JsonInclude]
-        [JsonPropertyName("Weather Information")]
-        public List<WeatherInformation>? m_WeatherInformation { get; set; }
-    }
+		[JsonInclude]
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		[JsonPropertyName("Previous Weather Stage")]
+		public WeatherStage Prev { get; set; }
 
-    public class WeatherInformation
-    {
-        [JsonInclude]
-        [JsonPropertyName("Day Information")]
-        public DayInformation? m_DayInformation { get; set; }
+		[JsonInclude]
+		[JsonPropertyName("Weather Information")]
+		public List<WeatherInformation>? m_WeatherInformation { get; set; }
+	}
 
-        [JsonInclude]
-        [JsonPropertyName("Weather Stage")]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public WeatherStage m_WeatherStage { get; set; }
-        [JsonInclude]
-        public float WindSpeed { get; set; }
-        [JsonInclude]
-        public float WindAngle { get; set; }
-        [JsonInclude]
-        public float WindPlayerMult { get; set; }
-        [JsonInclude]
-        public float Temperature { get; set; }
-    }
+	public class WeatherInformation
+	{
+		public WeatherInformation() { }
 
-    public class DayInformation
-    {
-        [JsonInclude]
-        public int Day { get; set; }
-        [JsonInclude]
-        public int Hour { get; set; }
-        [JsonInclude]
-        public int Minute { get; set; }
-    }
+		[JsonInclude]
+		[JsonPropertyName("Day Information")]
+		public DayInformation? m_DayInformation { get; set; }
+
+		[JsonInclude]
+		[JsonPropertyName("Weather Stage")]
+		[JsonConverter(typeof(JsonStringEnumConverter))]
+		public WeatherStage m_WeatherStage { get; set; }
+		[JsonInclude]
+		public float WindSpeed { get; set; }
+		[JsonInclude]
+		public float WindAngle { get; set; }
+		[JsonInclude]
+		public float WindPlayerMult { get; set; }
+		[JsonInclude]
+		public float Temperature { get; set; }
+	}
+
+	public class DayInformation
+	{
+		public DayInformation() { }
+
+		[JsonInclude]
+		public int Day { get; set; }
+		[JsonInclude]
+		public int Hour { get; set; }
+		[JsonInclude]
+		public int Minute { get; set; }
+	}
 
 
 }

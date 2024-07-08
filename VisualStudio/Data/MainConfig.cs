4df2ee9278bafc9520c715ed8c04924e4037af5e
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-07-08 20:16:23 -0300

    Update MainConfig.cs

--- VisualStudio/Data/MainConfig.cs
+++ VisualStudio/Data/MainConfig.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AuroraMonitor.Data
 {
-    public class MainConfig
-    {
-        public bool BaseLoaded { get; set; }
-        public bool SandboxLoaded { get; set; }
-        public bool DLC01Loaded { get; set; }
-        public string? BaseSceneName { get; set; }
-        public string? SandboxSceneName { get; set; }
-        public string? DLC01SceneName { get; set; }
-        public bool ModInitiliated { get; set; }
-    }
+	public class MainConfig
+	{
+		public MainConfig() { }
+
+		[JsonInclude]
+		public bool BaseLoaded { get; set; }
+		[JsonInclude]
+		public bool SandboxLoaded { get; set; }
+		[JsonInclude]
+		public bool DLC01Loaded { get; set; }
+		[JsonInclude]
+		public string? BaseSceneName { get; set; }
+		[JsonInclude]
+		public string? SandboxSceneName { get; set; }
+		[JsonInclude]
+		public string? DLC01SceneName { get; set; }
+		[JsonInclude]
+		public bool ModInitiliated { get; set; }
+		[JsonInclude]
+		public List<float>? WeatherWeights { get; set; } = new();
+	}
 }

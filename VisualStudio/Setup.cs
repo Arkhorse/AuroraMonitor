f8fbce599f19332f251d62a843084497994059cb
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-10-13 23:59:14 -0300

    Update Setup.cs

--- VisualStudio/Setup.cs
+++ VisualStudio/Setup.cs
@@ -1,11 +1,82 @@
 ﻿namespace AuroraMonitor
 {
+	/// <summary>
+	/// The Setup class is used to setup all file based stuff
+	/// </summary>
 	public class Setup
 	{
-		public static bool Init()
+		public async static Task<bool> Init()
 		{
-			SetupInstancedClasses();
-			SetupConfigurationData();
+			if (SetupInstancedClasses())
+			{
+			 	if (await SetupConfigurationData())
+				{
+					if (LoadAssets())
+					{
+						Main.Logger.Log($"Init Completed", FlaggedLoggingLevel.Debug);
+						return true;
+					}
+				}
+				else
+				{
+					Main.Logger.Log($"Attempting to setup config data failed", FlaggedLoggingLevel.Critical);
+				}
+			}
+			Main.Logger.Log($"Init Failed", FlaggedLoggingLevel.Debug);
+			return false;
+		}
+
+		// All of these should use the ??= thingy to ensure prexisting data is kept
+		private static bool SetupInstancedClasses()
+		{
+			Main.Config					??= new();
+			Main.SettingsInstance		??= new();
+			Main.MonitorData			??= new();
+			Main.WeatherDataTracking	??= new();
+			Main.WeatherIcons			??= new();
+
+			return true;
+		}
+
+		private static async Task<bool> SetupConfigurationData()
+		{
+			if (SetupFolders())
+			{
+				if (await LoadAllFiles())
+				{
+					if (Main.MonitorData != null && Main.Config != null && Main.WeatherDataTracking != null)
+					{
+						Main.Logger.Log("All configs successfully loaded", FlaggedLoggingLevel.Verbose);
+						return true;
+					}
+				}
+			}
+
+			Main.Logger.Log($"All or some configs failed to load: Monitor Data: {(Main.MonitorData == null ? "null" : "not null")} | Config: {(Main.Config == null ? "null" : "not null")} | Weather Tracking: {(Main.WeatherDataTracking == null ? "null" : "not null")}", FlaggedLoggingLevel.Warning);
+			return false;
+		}
+
+		private async static Task<bool> LoadAllFiles()
+		{
+			Main.MonitorData			= await JsonFile.LoadAsync<WeatherMonitorData>(Main.MonitorMainConfig, true);
+			Main.Config					= await JsonFile.LoadAsync<MainConfig>(Main.MonitorConfig, true);
+			Main.WeatherDataTracking	= await JsonFile.LoadAsync<WeatherDataTracking>(Main.WeatherTrackingFile, true);
+
+			return true;
+		}
+
+		private static bool LoadAssets()
+		{
+			foreach (string file in Main.WeatherIconNames)
+			{
+				Texture2D? texture = ImageUtilities.GetPNG("Monitor//Textures", file);
+				if (texture != null)
+				{
+					texture.name = file;
+					var _ = new TextureDefinition() { Name = file, Texture = texture };
+					Main.WeatherIcons.Add(_);
+				}
+			}
 
 			return true;
 		}
@@ -18,28 +89,6 @@
 			}
 
 			return true;
-		}
-
-		private static bool SetupInstancedClasses()
-		{
-			Main.Config ??= new();
-			Main.SettingsInstance ??= new();
-			Main.MonitorData ??= new();
-			Main.WeatherDataTracking ??= new();
-
-			return true;
-		}
-
-		private static bool SetupConfigurationData()
-		{
-			if (SetupFolders())
-			{
-				Main.MonitorData = JsonFile.Load<WeatherMonitorData>(Main.MonitorMainConfig, true);
-				Main.Config = JsonFile.Load<MainConfig>(Main.MonitorConfig, true);
-				return true;
-			}
-
-			return false;
 		}
 	}
 }
